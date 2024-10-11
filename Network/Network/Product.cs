86fdf79a20b8fadf05a25f0d5cfa3a0bfaee2751
Arina Pereteatcu <arinapereteatcu0@gmail.com>
2024-10-11 10:23:19 +0300

    Create class Product

---
+++ Network/Network/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newtork
+{
+    public class Product
+    {
+        public string? Name { get; set; }
+        public int Price { get; set; }
+        public string? Link { get; set; }
+        public string? Resolution { get; set; }
+    }
+}
