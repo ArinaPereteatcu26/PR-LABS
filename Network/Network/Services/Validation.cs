c8f03f50b5518401da7dec3946f3c59622edb26d
Arina Pereteatcu <arinapereteatcu0@gmail.com>
2024-10-13 15:47:00 +0300

    Add validations of data

---
+++ Network/Network/Services/Validation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Network.Services
+{
+    public class Validation
+    {
+        public decimal ValidProduct(string price)
+        {
+            // Remove whitespaces and validate conversion to decimal
+            var cleanedPrice = RemoveWhitespacesUsingRegex(price);
+
+            // Try parsing the cleaned price and handle any exceptions
+            if (decimal.TryParse(cleanedPrice, out decimal validProduct))
+            {
+                return validProduct;
+            }
+            else
+            {
+                throw new FormatException("Invalid price format.");
+            }
+        }
+
+        public string ValidMemory(string memory)
+        {
+            return RemoveWhitespacesUsingRegex(memory);
+        }
+
+        // Change the return type from bool to string
+        private string RemoveWhitespacesUsingRegex(string source)
+        {
+            return Regex.Replace(source, @"\s", string.Empty);
+        }
+    }
+}
--- Network/Network/obj/Debug/net6.0/Network.AssemblyInfo.cs
+++ Network/Network/obj/Debug/net6.0/Network.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Network")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8e91fb47f46caeb4458f64c657f0198e40da451")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f002aaaaf76151f2039a1de90bc84eb9461663bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Network")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Network")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
