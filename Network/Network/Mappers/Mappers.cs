3456c6797de32cefc54ffab4f04d64a78b768f9e
Arina Pereteatcu <arinapereteatcu0@gmail.com>
2024-10-13 20:03:07 +0300

    Add Serialization logic

--- Network/Network/Mappers/Mappers.cs
+++ Network/Network/Mappers/Mappers.cs
@@ -1,11 +1,4 @@
-﻿using Network.Filter;
-using Network.Models;
-using Network.Filter;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Network.Models;
 
 namespace Network.Mappers
 {
--- Network/Network/obj/Debug/net6.0/Network.AssemblyInfo.cs
+++ Network/Network/obj/Debug/net6.0/Network.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Network")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aea202325d3d4ec941661278efcc6244a8ec609f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a7fd276fd3c2160398350bf661f482227f1c988")]
 [assembly: System.Reflection.AssemblyProductAttribute("Network")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Network")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
