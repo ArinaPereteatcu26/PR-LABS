aea202325d3d4ec941661278efcc6244a8ec609f
Arina Pereteatcu <arinapereteatcu0@gmail.com>
2024-10-13 16:32:15 +0300

    Add class to Extract product

---
+++ Network/Network/Services/ExtractProduct.cs
@@ -0,0 +1,48 @@
+﻿using HtmlAgilityPack;
+namespace Network.Services
+{
+    public class ExtractProduct
+    {
+        private Validation _validation;
+        public ExtractProduct() 
+        {
+            _validation = new Validation();
+        }
+        public string ExtractName(HtmlNode productNode)
+        {
+            var nameNode = productNode.SelectSingleNode(".//div[contains(@class, 'grid-item')]//figcaption//a[contains(@class, 'ga-item')]");
+            string name = nameNode != null ? nameNode.InnerText.Trim() : string.Empty;
+            return name;
+        }
+        public decimal ExtractPrice(HtmlNode productNode)
+        {
+            var priceNode = productNode.SelectSingleNode(".//div[contains(@class, 'bottom-wrap')]//div[contains(@class, 'price-wrap')]//span[contains(@class, 'price-new')]/b");
+            string price = priceNode != null ? priceNode.InnerText.Trim() : string.Empty;
+            return _validation.ValidProduct(price.Trim());
+        }
+        public string ExtractProductMemory(string htmlContent)
+        {
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(htmlContent);
+            var productNode = htmlDoc.DocumentNode.SelectSingleNode("\".//div[contains(@class, 'grid-item')]//figcaption//a[contains(@class, 'ga-item')]\"\r\n");
+           
+            if (productNode != null)
+            {
+                var features = productNode.SelectNodes(".//li");
+                if (features != null)
+                {
+                    foreach (var feature in features)
+                    {
+                        if (feature.InnerText.Trim().Contains("Memorie"))
+                        {
+                            var memoryDescription = feature.InnerText.Trim();
+                            return _validation.ValidMemory(memoryDescription.Split(":")[1]);
+                        }
+                    }
+                }
+            }
+            return "Memory not found";
+        }
+
+    }
+}
--- Network/Network/obj/Debug/net6.0/Network.AssemblyInfo.cs
+++ Network/Network/obj/Debug/net6.0/Network.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Network")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8f03f50b5518401da7dec3946f3c59622edb26d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01fbeed80f0578c119534bc4d148100ea48f5a96")]
 [assembly: System.Reflection.AssemblyProductAttribute("Network")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Network")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
