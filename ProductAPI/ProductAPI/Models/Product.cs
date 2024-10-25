5d91edfbef0b3137d0e386dccaafea8e3274520c
Arina <arinapereteatcu0@gmail.com>
2024-10-25 17:53:48 +0300

    Create Product data model

---
+++ ProductAPI/ProductAPI/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public string ProductLink { get; set; }
+    public string Year { get; set; }
+}
